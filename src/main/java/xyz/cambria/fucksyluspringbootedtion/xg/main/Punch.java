ec31d7160855a0bda518b1c5dcc42e34a2a88bb6
Cambria_D <835014916@qq.com>
2022-01-04 12:23:26 +0800

    修复了properties对象忘记clear的bug

--- src/main/java/xyz/cambria/fucksyluspringbootedtion/xg/main/Punch.java
+++ src/main/java/xyz/cambria/fucksyluspringbootedtion/xg/main/Punch.java
@@ -35,6 +35,62 @@ public class Punch {
 
         for (File file : files) {
 //            System.out.println(file.getName());
+            log.info("{}" , file.getName());
+        }
+        Properties properties = new Properties();
+
+        for (File file : files) {
+
+            properties.load(new FileReader(file));
+
+//          System.out.println(properties.getProperty("id"));
+            String cookie = XGLogin.login(properties.getProperty("id"), properties.getProperty("pwd"));
+
+            if (cookie == null) {
+                properties.clear();
+                continue;
+            }
+            try {
+                Temperature.run("0" , null , cookie);
+                Temperature.run("8" , null , cookie);
+                Temperature.run("16" , null , cookie);
+                log.info("{} upload temperatrue success" , file.getName());
+
+            } catch (Exception e) {
+                log.error("{} upload temperatrue Failed." , file.getName());
+                properties.clear();
+                e.printStackTrace();
+            }
+
+            if (properties.getProperty("IDNum") == null) {
+                log.info("no userinfo for infocollect.");
+                properties.clear();
+                continue;
+            }
+            try {
+                InfoCollect.run(cookie , properties , GetInfoCollectForm.run(cookie));
+                properties.clear();
+            } catch (Exception e) {
+//                    System.out.println("没抓到健康状况表单信息,timestamp:" + System.currentTimeMillis());
+                properties.clear();
+                log.error("{} get infocollect form failed." , file.getName());
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String FILE_PATH = FilePathUtil.getTemperaturePath();
+        File[] files = new File(FILE_PATH).listFiles();
+//        System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
+//        System.out.println(FILE_PATH + " Found Files:");
+        log.info("Upload Temperature info at {}" , new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
+        log.info("Found Files:");
+        if (files == null)
+            return;
+
+        for (File file : files) {
+//            System.out.println(file.getName());
             log.info("excute with {}" , file.getName());
         }
         Properties properties = new Properties();
@@ -73,5 +129,4 @@ public class Punch {
 
         }
     }
-
 }
